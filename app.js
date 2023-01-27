488ff28798d18d085c7c4bfd10af9173f50322f9
NHIMCOII <90151567+NHIMCOII@users.noreply.github.com>
2023-01-27 13:39:35 +0700

    minor update

--- app.js
+++ app.js
@@ -15,28 +15,31 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credential: true,
+	origin: '*',
+	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+	credential: true,
 };
 
 app.use(express.static(path.join(__dirname, 'images')));
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use(
-  cookieSession({
-    signed: false,
-    secure: false,
-  })
+	cookieSession({
+		signed: false,
+		secure: false,
+	})
 );
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 
 mongoose
-  .connect(process.env.MONGO_DATABASE)
-  .then(app.listen(3000))
-  .catch((err) => {
-    console.log(err);
-  });
+	.connect(process.env.MONGO_DATABASE)
+	.then(() => {
+		app.listen(3000);
+		console.log('============ Database Connected ==========');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
